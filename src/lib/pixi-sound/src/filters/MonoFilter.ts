69bacc7386fd52bbf6c5213124d64543ef576ae7
Victor Debone <victor@debone.com.br>
2026-02-03 23:04:44 +0100

    RATZ-105 feat: forking @pixi/sound

---
+++ src/lib/pixi-sound/src/filters/MonoFilter.ts
@@ -0,0 +1,34 @@
+import { getInstance } from '../instance';
+import { Filter } from './Filter';
+
+/**
+ * Combine all channels into mono channel.
+ *
+ * @memberof filters
+ */
+class MonoFilter extends Filter {
+  /** Merger node */
+  private _merger: ChannelMergerNode;
+
+  constructor() {
+    let merger: ChannelMergerNode;
+    let splitter: ChannelSplitterNode;
+
+    const { audioContext } = getInstance().context;
+
+    splitter = audioContext.createChannelSplitter();
+    merger = audioContext.createChannelMerger();
+    merger.connect(splitter);
+
+    super(merger, splitter);
+    this._merger = merger;
+  }
+
+  public destroy(): void {
+    this._merger?.disconnect();
+    this._merger = null!;
+    super.destroy();
+  }
+}
+
+export { MonoFilter };
---
+++ src/lib/pixi-sound/src/index.ts
@@ -0,0 +1,13 @@
+import * as filters from './filters';
+import * as htmlaudio from './htmlaudio';
+import * as utils from './utils';
+import * as webaudio from './webaudio';
+
+export * from './Filterable';
+export * from './filters/Filter';
+export * from './interfaces';
+export * from './Sound';
+export * from './soundAsset';
+export * from './SoundLibrary';
+export * from './SoundSprite';
+export { filters, htmlaudio, utils, webaudio };
